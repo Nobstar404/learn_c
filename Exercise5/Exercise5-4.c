1cbe65ea714ca542f18f5203a1c2ef5193129353
Nobstar404 <iqbalfadhillah973@gmail.com>
2026-08-18 16:57:31 +0700

    Exercise5-4

---
+++ Exercise5/Exercise5-4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int strend(const char* s, const char* t);
+
+int main()
+{
+    const char* s1 = "hello 0xff";
+    const char* s2 = "0xff";
+
+    printf("%d!\n", strend(s1, s2));
+
+    return 0;
+}
+
+int strend(const char* s, const char* t)
+{
+    while(*s) s++;
+    size_t i;
+    for(i = 0; *t; i++, t++);
+
+    if(*(s-1) == *(t-1))
+    {
+        for( ; *s == *t; s--, t--, i--)
+            if(i == 0) return 1;
+    }
+
+    return 0;
+}
